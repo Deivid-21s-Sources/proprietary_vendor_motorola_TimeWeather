08560da0a75af8ac7415798d949552146d8debcd
Deivid21 <david.parra.ignacio@gmail.com>
2024-08-19 19:20:05 -0400

    TimeWeather: Update blobs from "motorola/xpeng_g/xpeng:11/RRX31.Q3-59-42/d645d1:user/release-keys"
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- timeweather.mk
+++ timeweather.mk
@@ -4,6 +4,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/TimeWeather
  
 PRODUCT_COPY_FILES += \
+    vendor/motorola/TimeWeather/proprietary/product/etc/default-permissions/default-permission-com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permission-com.motorola.timeweatherwidget.xml \
     vendor/motorola/TimeWeather/proprietary/product/etc/permissions/com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.timeweatherwidget.xml \
     vendor/motorola/TimeWeather/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.timeweatherwidget.xml
 

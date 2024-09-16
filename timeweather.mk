3d713755ee3c2f73fc9cb8feb79c07bfbe06e872
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-15 23:45:04 -0300

    TimeWeather: makefiles
    Change-Id: I89b9dbdcc505212036b7c33e9f601c9323223930

--- timeweather.mk
+++ timeweather.mk
@@ -1,14 +1,32 @@
-# Automatically generated file. DO NOT MODIFY
+#
+# Copyright (C) 2008 - 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# SOONG NAMESPACES
 PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/TimeWeather
- 
+
+# Moto Signature App
+$(call inherit-product, vendor/motorola/MotoSignatureApp/motosignatureapp.mk)
+
+# Moto Time Weather
+# Version: 4.10.35
 PRODUCT_COPY_FILES += \
     vendor/motorola/TimeWeather/proprietary/product/etc/default-permissions/default-permission-com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permission-com.motorola.timeweatherwidget.xml \
     vendor/motorola/TimeWeather/proprietary/product/etc/permissions/com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.timeweatherwidget.xml \
     vendor/motorola/TimeWeather/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.timeweatherwidget.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.timeweatherwidget.xml
-
-$(call inherit-product, vendor/motorola/MotoSignatureApp/motosignatureapp.mk)
 
 PRODUCT_PACKAGES += \
     TimeWeather
